7206e35416f034726c3a6dab8c073799a0c5aef9
David Black <david_black_uk@yahoo.com>
2019-03-21 13:52:23 +0000

    Removed use of scan liones by the ldi blit buffer

--- back_buffer.z80.asm
+++ back_buffer.z80.asm
@@ -464,54 +464,54 @@ off_by_one: .word 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 ; for Screen address 0 ($4000) maps to char_line_00 contained in here
 ;
 screen_map:		
-	.word $4000, scan_line_000, $4100, scan_line_001, $4200, scan_line_002, $4300, scan_line_003
-	.word $4400, scan_line_004, $4500, scan_line_005, $4600, scan_line_006, $4700, scan_line_007
-	.word $4020, scan_line_008, $4120, scan_line_009, $4220, scan_line_010, $4320, scan_line_011
-	.word $4420, scan_line_012, $4520, scan_line_013, $4620, scan_line_014, $4720, scan_line_015
-	.word $4040, scan_line_016, $4140, scan_line_017, $4240, scan_line_018, $4340, scan_line_019
-	.word $4440, scan_line_020, $4540, scan_line_021, $4640, scan_line_022, $4740, scan_line_023
-	.word $4060, scan_line_024, $4160, scan_line_025, $4260, scan_line_026, $4360, scan_line_027
-	.word $4460, scan_line_028, $4560, scan_line_029, $4660, scan_line_030, $4760, scan_line_031
-	.word $4080, scan_line_032, $4180, scan_line_033, $4280, scan_line_034, $4380, scan_line_035
-	.word $4480, scan_line_036, $4580, scan_line_037, $4680, scan_line_038, $4780, scan_line_039
-	.word $40A0, scan_line_040, $41A0, scan_line_041, $42A0, scan_line_042, $43A0, scan_line_043
-	.word $44A0, scan_line_044, $45A0, scan_line_045, $46A0, scan_line_046, $47A0, scan_line_047
-	.word $40C0, scan_line_048, $41C0, scan_line_049, $42C0, scan_line_050, $43C0, scan_line_051
-	.word $44C0, scan_line_052, $45C0, scan_line_053, $46C0, scan_line_054, $47C0, scan_line_055
-	.word $40E0, scan_line_056, $41E0, scan_line_057, $42E0, scan_line_058, $43E0, scan_line_059
-	.word $44E0, scan_line_060, $45E0, scan_line_061, $46E0, scan_line_062, $47E0, scan_line_063
-	.word $4800, scan_line_064, $4900, scan_line_065, $4A00, scan_line_066, $4B00, scan_line_067
-	.word $4C00, scan_line_068, $4D00, scan_line_069, $4E00, scan_line_070, $4F00, scan_line_071
-	.word $4820, scan_line_072, $4920, scan_line_073, $4A20, scan_line_074, $4B20, scan_line_075
-	.word $4C20, scan_line_076, $4D20, scan_line_077, $4E20, scan_line_078, $4F20, scan_line_079
-	.word $4840, scan_line_080, $4940, scan_line_081, $4A40, scan_line_082, $4B40, scan_line_083
-	.word $4C40, scan_line_084, $4D40, scan_line_085, $4E40, scan_line_086, $4F40, scan_line_087
-	.word $4860, scan_line_088, $4960, scan_line_089, $4A60, scan_line_090, $4B60, scan_line_091
-	.word $4C60, scan_line_092, $4D60, scan_line_093, $4E60, scan_line_094, $4F60, scan_line_095
-	.word $4880, scan_line_096, $4980, scan_line_097, $4A80, scan_line_098, $4B80, scan_line_099
-	.word $4C80, scan_line_100, $4D80, scan_line_101, $4E80, scan_line_102, $4F80, scan_line_103
-	.word $48A0, scan_line_104, $49A0, scan_line_105, $4AA0, scan_line_106, $4BA0, scan_line_107
-	.word $4CA0, scan_line_108, $4DA0, scan_line_109, $4EA0, scan_line_110, $4FA0, scan_line_111
-	.word $48C0, scan_line_112, $49C0, scan_line_113, $4AC0, scan_line_114, $4BC0, scan_line_115
-	.word $4CC0, scan_line_116, $4DC0, scan_line_117, $4EC0, scan_line_118, $4FC0, scan_line_119
-	.word $48E0, scan_line_120, $49E0, scan_line_121, $4AE0, scan_line_122, $4BE0, scan_line_123
-	.word $4CE0, scan_line_124, $4DE0, scan_line_125, $4EE0, scan_line_126, $4FE0, scan_line_127
-	.word $5000, scan_line_128, $5100, scan_line_129, $5200, scan_line_130, $5300, scan_line_131
-	.word $5400, scan_line_132, $5500, scan_line_133, $5600, scan_line_134, $5700, scan_line_135
-	.word $5020, scan_line_136, $5120, scan_line_137, $5220, scan_line_138, $5320, scan_line_139
-	.word $5420, scan_line_140, $5520, scan_line_141, $5620, scan_line_142, $5720, scan_line_143
-	.word $5040, scan_line_144, $5140, scan_line_145, $5240, scan_line_146, $5340, scan_line_147
-	.word $5440, scan_line_148, $5540, scan_line_149, $5640, scan_line_150, $5740, scan_line_151
-	.word $5060, scan_line_152, $5160, scan_line_153, $5260, scan_line_154, $5360, scan_line_155
-	.word $5460, scan_line_156, $5560, scan_line_157, $5660, scan_line_158, $5760, scan_line_159
-	.word $5080, scan_line_160, $5180, scan_line_161, $5280, scan_line_162, $5380, scan_line_163
-	.word $5480, scan_line_164, $5580, scan_line_165, $5680, scan_line_166, $5780, scan_line_167
-	.word $50A0, scan_line_168, $51A0, scan_line_169, $52A0, scan_line_170, $53A0, scan_line_171
-	.word $54A0, scan_line_172, $55A0, scan_line_173, $56A0, scan_line_174, $57A0, scan_line_175
-	.word $50C0, scan_line_176, $51C0, scan_line_177, $52C0, scan_line_178, $53C0, scan_line_179
-	.word $54C0, scan_line_180, $55C0, scan_line_181, $56C0, scan_line_182, $57C0, scan_line_183
-	.word $50E0, scan_line_184, $51E0, scan_line_185, $52E0, scan_line_186, $53E0, scan_line_187
-	.word $54E0, scan_line_188, $55E0, scan_line_189, $56E0, scan_line_190, $57E0, scan_line_191
+.word $4000, char_line_00 + 0,    $4100, char_line_00 + 32,   $4200, char_line_00 + 64,  $4300, char_line_00 + 96
+.word $4400, char_line_00 + 128,  $4500, char_line_00 + 160,  $4600, char_line_00 + 192,  $4700, char_line_00 + 224
+.word $4020, char_line_00 + 256,  $4120, char_line_00 + 288,  $4220, char_line_00 + 320,  $4320, char_line_00 + 352
+.word $4420, char_line_00 + 384,  $4520, char_line_00 + 416,  $4620, char_line_00 + 448,  $4720, char_line_00 + 480
+.word $4040, char_line_00 + 512,  $4140, char_line_00 + 544,  $4240, char_line_00 + 576,  $4340, char_line_00 + 608
+.word $4440, char_line_00 + 640,  $4540, char_line_00 + 672,  $4640, char_line_00 + 704,  $4740, char_line_00 + 736
+.word $4060, char_line_00 + 768,  $4160, char_line_00 + 800,  $4260, char_line_00 + 832,  $4360, char_line_00 + 864
+.word $4460, char_line_00 + 896,  $4560, char_line_00 + 928,  $4660, char_line_00 + 960,  $4760, char_line_00 + 992
+.word $4080, char_line_00 + 1024, $4180, char_line_00 + 1056, $4280, char_line_00 + 1088, $4380, char_line_00 + 1120
+.word $4480, char_line_00 + 1152, $4580, char_line_00 + 1184, $4680, char_line_00 + 1216, $4780, char_line_00 + 1248
+.word $40A0, char_line_00 + 1280, $41A0, char_line_00 + 1312, $42A0, char_line_00 + 1344, $43A0, char_line_00 + 1376
+.word $44A0, char_line_00 + 1408, $45A0, char_line_00 + 1440, $46A0, char_line_00 + 1472, $47A0, char_line_00 + 1504
+.word $40C0, char_line_00 + 1536, $41C0, char_line_00 + 1568, $42C0, char_line_00 + 1600, $43C0, char_line_00 + 1632
+.word $44C0, char_line_00 + 1664, $45C0, char_line_00 + 1696, $46C0, char_line_00 + 1728, $47C0, char_line_00 + 1760
+.word $40E0, char_line_00 + 1792, $41E0, char_line_00 + 1824, $42E0, char_line_00 + 1856, $43E0, char_line_00 + 1888
+.word $44E0, char_line_00 + 1920, $45E0, char_line_00 + 1952, $46E0, char_line_00 + 1984, $47E0, char_line_00 + 2016
+.word $4800, char_line_00 + 2048, $4900, char_line_00 + 2080, $4A00, char_line_00 + 2112, $4B00, char_line_00 + 2144
+.word $4C00, char_line_00 + 2176, $4D00, char_line_00 + 2208, $4E00, char_line_00 + 2240, $4F00, char_line_00 + 2272
+.word $4820, char_line_00 + 2304, $4920, char_line_00 + 2336, $4A20, char_line_00 + 2368, $4B20, char_line_00 + 2400
+.word $4C20, char_line_00 + 2432, $4D20, char_line_00 + 2464, $4E20, char_line_00 + 2496, $4F20, char_line_00 + 2528
+.word $4840, char_line_00 + 2560, $4940, char_line_00 + 2592, $4A40, char_line_00 + 2624, $4B40, char_line_00 + 2656
+.word $4C40, char_line_00 + 2688, $4D40, char_line_00 + 2720, $4E40, char_line_00 + 2752, $4F40, char_line_00 + 2784
+.word $4860, char_line_00 + 2816, $4960, char_line_00 + 2848, $4A60, char_line_00 + 2880, $4B60, char_line_00 + 2912
+.word $4C60, char_line_00 + 2944, $4D60, char_line_00 + 2976, $4E60, char_line_00 + 3008, $4F60, char_line_00 + 3040
+.word $4880, char_line_00 + 3072, $4980, char_line_00 + 3104, $4A80, char_line_00 + 3136, $4B80, char_line_00 + 3168
+.word $4C80, char_line_00 + 3200, $4D80, char_line_00 + 3232, $4E80, char_line_00 + 3264, $4F80, char_line_00 + 3296
+.word $48A0, char_line_00 + 3328, $49A0, char_line_00 + 3360, $4AA0, char_line_00 + 3392, $4BA0, char_line_00 + 3424
+.word $4CA0, char_line_00 + 3456, $4DA0, char_line_00 + 3488, $4EA0, char_line_00 + 3520, $4FA0, char_line_00 + 3552
+.word $48C0, char_line_00 + 3584, $49C0, char_line_00 + 3616, $4AC0, char_line_00 + 3648, $4BC0, char_line_00 + 3680
+.word $4CC0, char_line_00 + 3712, $4DC0, char_line_00 + 3744, $4EC0, char_line_00 + 3776, $4FC0, char_line_00 + 3808
+.word $48E0, char_line_00 + 3840, $49E0, char_line_00 + 3872, $4AE0, char_line_00 + 3904, $4BE0, char_line_00 + 3936
+.word $4CE0, char_line_00 + 3968, $4DE0, char_line_00 + 4000, $4EE0, char_line_00 + 4032, $4FE0, char_line_00 + 4064
+.word $5000, char_line_00 + 4096, $5100, char_line_00 + 4128, $5200, char_line_00 + 4160, $5300, char_line_00 + 4192
+.word $5400, char_line_00 + 4224, $5500, char_line_00 + 4256, $5600, char_line_00 + 4288, $5700, char_line_00 + 4320
+.word $5020, char_line_00 + 4352, $5120, char_line_00 + 4384, $5220, char_line_00 + 4416, $5320, char_line_00 + 4448
+.word $5420, char_line_00 + 4480, $5520, char_line_00 + 4512, $5620, char_line_00 + 4544, $5720, char_line_00 + 4576
+.word $5040, char_line_00 + 4608, $5140, char_line_00 + 4640, $5240, char_line_00 + 4672, $5340, char_line_00 + 4704
+.word $5440, char_line_00 + 4736, $5540, char_line_00 + 4768, $5640, char_line_00 + 4800, $5740, char_line_00 + 4832
+.word $5060, char_line_00 + 4864, $5160, char_line_00 + 4896, $5260, char_line_00 + 4928, $5360, char_line_00 + 4960
+.word $5460, char_line_00 + 4992, $5560, char_line_00 + 5024, $5660, char_line_00 + 5056, $5760, char_line_00 + 5088
+.word $5080, char_line_00 + 5120, $5180, char_line_00 + 5152, $5280, char_line_00 + 5184, $5380, char_line_00 + 5216
+.word $5480, char_line_00 + 5248, $5580, char_line_00 + 5280, $5680, char_line_00 + 5312, $5780, char_line_00 + 5344
+.word $50A0, char_line_00 + 5376, $51A0, char_line_00 + 5408, $52A0, char_line_00 + 5440, $53A0, char_line_00 + 5472
+.word $54A0, char_line_00 + 5504, $55A0, char_line_00 + 5536, $56A0, char_line_00 + 5568, $57A0, char_line_00 + 5600
+.word $50C0, char_line_00 + 5632, $51C0, char_line_00 + 5664, $52C0, char_line_00 + 5696, $53C0, char_line_00 + 5728
+.word $54C0, char_line_00 + 5760, $55C0, char_line_00 + 5792, $56C0, char_line_00 + 5824, $57C0, char_line_00 + 5856
+.word $50E0, char_line_00 + 5888, $51E0, char_line_00 + 5920, $52E0, char_line_00 + 5952, $53E0, char_line_00 + 5984
+.word $54E0, char_line_00 + 6016, $55E0, char_line_00 + 6048, $56E0, char_line_00 + 6080, $57E0, char_line_00 + 6112
 
 
 
